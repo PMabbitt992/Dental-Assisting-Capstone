831fa38d6478ecae5792d6ebb9298bba68c57347
Connor Webdale <CWebdale006@west-mec.org>
2019-05-01 15:25:52 -0700

    Beginning work on the image timer
    Added images and the structure of some functions to begin work on changing them on a timer

--- Examples/main.js
+++ Examples/main.js
@@ -1,1 +1,30 @@
 "use strict";
+
+// Working to change images 
+function topLeftImg() {
+    var currentImg = document.getElementById("topLeft");
+    var images = ["/Examples/timer_images/check-up.jpg"];
+    console.log(currentImg);
+    currentImg
+}
+
+function botLeftImg() {
+
+}
+
+function topRightImg() {
+
+}
+
+function botRightImg() {
+
+}
+
+
+
+
+
+window.setInterval(topLeftImg, 5000);
+window.setInterval(botLeftImg, 5500);
+window.setInterval(topRightImg, 7000);
+window.setInterval(botRightImg, 7500);
